0d08635c0d9314fe999a5b8fe1c5d3a30bf968f4
Ryan Aubrey <rma7qb@virginia.edu>
2020-04-21 20:17:36 -0400

    ConfigMaps - using env configMapRef, fromEnv, and volumeMounts to pass configMaps to containers

--- containers/rubytest/app.rb
+++ containers/rubytest/app.rb
@@ -4,14 +4,8 @@ require "sinatra"
 
 set :bind, "0.0.0.0"
 
-$counter = 0
-
 get "*" do
-  $counter += 1
-  if $counter > 3
-    raise "Whoops, something is wrong.. wonder where this goes"
-  end
-  
-  "[v3] Hello Kubernetes!\n"
+  message = ENV.fetch("MESSAGE", "Hello, Kubernetes")
+  "[v4] #{message} (from #{`hostname`.strip})\n"
 end
 
